bb0ad48eb91677ea1d21d4468fd9191ee88d4d20
Mehedi hasan ronm <hassan.mehedi2026@gmail.com>
2023-12-04 20:55:52 +0600

    added simple form section use form tag and onsubmit handler with event (e)

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import SimpleForm from './Components/SimpleForm/SimpleForm'
+
+function App() {
+
+
+  return (
+    <>
+      
+      
+        <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,23 @@
+
+
+const SimpleForm = () => {
+    const handleSubmit = e =>{
+        e.preventDefault();
+        console.log(e.target.name.value)
+        console.log(e.target.email.value)
+        console.log(e)
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" />
+                <br />
+                <input type="email" name="email" id="" />
+                <br />
+                <input type="submit" />
+            </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
