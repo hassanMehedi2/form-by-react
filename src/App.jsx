ae9640a2b3ffd15f68fe53cf06e53a7ef7f1ea12
Mehedi hasan ronm <hassan.mehedi2026@gmail.com>
2023-12-07 21:26:40 +0600

    added Reusable form

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@
 import './App.css'
 import HookForm from './Components/HookForn/HookForm'
 import RefForm from './Components/RefForm/RefForm'
+import ReusableForm from './Components/ReusableForm/ReusableForm'
 import SimpleForm from './Components/SimpleForm/SimpleForm'
 import StatefulForm from './Components/StatefulForm/StatefulForm'
 
 function App() {
-
+  const handleSignUpSubmit = data=>{
+    console.log('signup data '+ data.name+data.email);
+  }
+  const handleUpdateSubmit = data=>{
+    console.log('Update data '+ data.name+data.email);
+  }
 
   return (
     <>
@@ -15,7 +21,25 @@ function App() {
         {/* <SimpleForm></SimpleForm> */}
         {/* <StatefulForm></StatefulForm> */}
         {/* <RefForm></RefForm> */}
-        <HookForm></HookForm>
+        {/* <HookForm></HookForm> */}
+        <ReusableForm 
+          formTitle={'Sign UP'} 
+          handleSubmit={handleSignUpSubmit}>
+          {/* // children prop  */}
+          <div>
+            <h2>sign Up</h2>
+            <p>please Sign Up</p>
+          </div>
+          </ReusableForm>
+        <br />
+        <ReusableForm formTitle={'Update Profile'}
+          handleSubmit={handleUpdateSubmit}
+          submitBtnText='Update'>
+            <div>
+            <h2>Update</h2>
+            <p>please Update</p>
+          </div>
+          </ReusableForm>
     </>
   )
 }
